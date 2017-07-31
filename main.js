e68ec952d4b083f38892f849b10173d135e47823
askflow1111 <askflow1111@hotmail.com>
2017-07-31 00:58:51 -0400

    added tictactoe

---
+++ main.js
@@ -0,0 +1,12 @@
+let currentPlayer = "X",
+board = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+container = document.querySelector("#container"),
+$winner = document.querySelector("#winner");
+
+const main = box => {
+if ($winner.innerHTML !== "" || box.value !== "") return;
+box.value = currentPlayer;
+board[box.name] = currentPlayer;
+((board[0] === board[1] && board[1] === board[2]) || (board[3] === board[4] && board[4] === board[5]) || (board[6] === board[7] && board[7] === board[8]) || (board[0] === board[3] && board[3] === board[6]) || (board[1] === board[4] && board[4] === board[7]) || (board[2] === board[5] && board[5] === board[8]) || (board[0] === board[4] && board[4] === board[8]) || (board[2] === board[4] && board[4] === board[6]) ) && ($winner.innerHTML = `${currentPlayer} has won!`)
+currentPlayer = (currentPlayer === "X" && "O") || "X";
+}
