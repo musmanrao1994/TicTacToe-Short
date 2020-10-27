bc35a8b1885de23f9d682091e3007b7f0963db9e
Muhammad Usman <musmanraodev@gmail.com>
2020-10-27 03:35:41 -0400

    created shortest minimax tic tac toe

--- main.js
+++ main.js
@@ -1,8 +1,64 @@
-let currentPlayer = "X", board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-const main = box => {
-if (document.querySelector("#winner").innerHTML !== "" || box.value !== "") return;
-box.value = currentPlayer;
-board[box.name] = currentPlayer;
-((board[0] === board[1] && board[1] === board[2]) || (board[3] === board[4] && board[4] === board[5]) || (board[6] === board[7] && board[7] === board[8]) || (board[0] === board[3] && board[3] === board[6]) || (board[1] === board[4] && board[4] === board[7]) || (board[2] === board[5] && board[5] === board[8]) || (board[0] === board[4] && board[4] === board[8]) || (board[2] === board[4] && board[4] === board[6]) ) && (document.querySelector("#winner").innerHTML = `${currentPlayer} has won!`)
-currentPlayer = (currentPlayer === "X" && "O") || "X";
+let currentPlayer = "X",
+  board = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  resultDiv = document.querySelector(".result"),
+  allInputs = document.querySelectorAll(`input`),
+  hasGameEnded = false;
+
+[...allInputs].forEach((item, index) =>
+  item.addEventListener("click", () => handleInputClick(index))
+);
+
+const handleInputClick = (index) => {
+  if (isNaN(board[index]) || hasGameEnded) return;
+  allInputs[index].value = currentPlayer;
+  board[index] = currentPlayer;
+  (((isThereWinner(board) && (resultDiv.innerHTML = `${currentPlayer} has won!`)) || (isTie(board) && (resultDiv.innerHTML = `It is a tie!`))) && (hasGameEnded = true));
+  currentPlayer = currentPlayer === "X" ? "O" : "X"
+  if (currentPlayer === "O") AITurn(board);
+};
+
+function AITurn(arr) {
+  let bestScores = -Infinity;
+  let bestIndex = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      arr[i] = "O";
+      const score = miniMax(arr);
+      arr[i] = i;
+
+      if (score > bestScores) {
+        bestScores = score;
+        bestIndex = i;
+      }
+    }
+  }
+
+  allInputs[bestIndex].click();
 }
+
+function miniMax(arr, isAITurn = false) {
+  if (isThereWinner(arr)) {
+    return isAITurn ? -1 : 1;
+  } else if (isTie(arr)) {
+    return 0;
+  }
+
+  let bestScores = isAITurn ? -Infinity : Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      arr[i] = isAITurn ? "O" : "X";
+      const score = miniMax(arr, !isAITurn);
+      bestScores = isAITurn ? Math.max(score, bestScores) : Math.min(score, bestScores);
+      arr[i] = i;
+    }
+  }
+  return bestScores;
+}
+
+const isEqual = (...arr) => arr.every(item => item === arr[0]);
+
+const isThereWinner = (arr) => (isEqual(arr[0], arr[1], arr[2]) || isEqual(arr[3], arr[4], arr[5]) || isEqual(arr[6], arr[7], arr[8]) || isEqual(arr[0], arr[3], arr[6]) || isEqual(arr[1], arr[4], arr[7]) || isEqual(arr[2], arr[5], arr[8]) || isEqual(arr[0], arr[4], arr[8]) || isEqual(arr[2], arr[4], arr[6]));
+
+const isTie = (arr) => arr.every((item) => isNaN(item));
